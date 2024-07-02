bfc3341fd014533592b123b163af6f954f209016
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-07-02 11:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
-if (typeof s1 === 'string' && s1.length === 1;) {
-for (let j = 0; j < s1.length; j++) {
-	return j[0];
+if (s1&&s2) {
+	return s1[0] + s2[0];
+}else{
+	return '';
 }
-}else if(typeof s2 === 'string' && s2.length === 1;){
-	for (let i = 0; i < s2.length; i++) {
-		return i[0];
-	}
-}	
-}
+	
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
